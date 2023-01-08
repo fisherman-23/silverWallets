2014c7d3e41a91d50e1fd2f3b0efb8b3cbdf7cc7
fisherman-23 <ooijs234@gmail.com>
2023-01-08 22:29:50 +0800

    connected buttons to switch screens, completed login validation

--- compPT.py
+++ compPT.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 from kivy.properties import StringProperty, ObjectProperty
@@ -23,14 +24,63 @@ default_app = firebase_admin.initialize_app(cred_obj, {
 # app = firebase_admin.initialize_app();
 db = firestore.client();
 
+userPW = '' 
 userEmail = ''
 
 def stringToDict(x):
     dictionary = dict(subString.split("=") for subString in x.split(";"))
     return(dictionary)
 
+class WindowManager(ScreenManager):
+    pass
+
 class StartScreen(Screen):
     pass
+
+def LogInCheck(x,y):
+    doc_ref = db.collection(u'accounts').document(x)
+    if doc_ref.get().exists:
+        #now check for password 
+        data = doc_ref.get().to_dict()
+        pw = data.get(' pw ')
+        #print(pw)
+        #print(type(pw))
+
+        if pw.strip() == y: #TAKE Note: firebase stores values with spacings use strip
+            #print('true',pw,type(pw))
+            return True
+        else:
+            #print('false',pw,type(pw),y)
+            return False
+            
+    else:
+        return False
+    
+class LogInScreen(Screen):
+    status_info = StringProperty("")
+    def on_text_validate_email(self, widget):
+        self.text_input_email = widget.text.strip()
+    def on_text_validate_pw(self, widget):
+        self.text_input_pw = widget.text.strip()
+    def signup_to_firebase(self):
+        global userEmail
+        global userPW
+        #print(self.text_input_email)
+        #ref = db.collection(u'accounts').document(u'dMo8Os9D5xwAWwgNtqIr');
+        try:
+            if self.text_input_email == '' or self.text_input_pw == '':
+                print('empty fields')
+                self.status_info = "Error! Empty fields."
+            else:
+                if LogInCheck(self.text_input_email, self.text_input_pw) == True:
+                    self.status_info = "Correct, logging in"
+                    
+                else:
+                    self.status_info = "Incorrect Details"
+                    
+        except AttributeError:
+            print('a',self.text_input_email,self.text_input_pw)
+            self.status_info = "Please fill in the fields"
 
 class SignUpScreen(Screen): #screen property allows switch between, layout nested inside
     email_info = StringProperty("")
@@ -49,6 +99,8 @@ class SignUpScreen(Screen): #screen property allows switch between, layout neste
         print(self.text_input_target)
     
     def signup_to_firebase(self):
+        global userEmail
+        global userPW
         #print(self.text_input_email)
         #ref = db.collection(u'accounts').document(u'dMo8Os9D5xwAWwgNtqIr');
         try:
@@ -63,14 +115,19 @@ class SignUpScreen(Screen): #screen property allows switch between, layout neste
                     self.status_info = "Error! Already exists, try logging in"
                 else:
                     ref.document(self.text_input_email).set(stringToDict(str));
+                    userEmail = self.text_input_email
+                    userPW =  self.text_input_pw
+                    self.status_info = "Success!"
         except AttributeError:
             self.status_info = "Please fill in the fields"
         
-        
+class HomeScreen(Screen):
+    pass
 
 class MainWidget(Widget):
     pass
 
 class SilverWalletsApp(App):
     pass
+
 SilverWalletsApp().run()
